cbbc3a4afb036ac0a9019479f9aca9900d661be8
Sabik52 <rahman52neajur@gmail.com>
2022-06-01 05:50:43 +0600

    orders edited

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,36 +1,47 @@
 import React, { useState } from 'react';
 import useTool from '../../hooks/useTools';
 import Tool from '../Home/Tool/Tool';
+import Tools from '../Home/Tools/Tools';
 
 import Footer from '../Shared/Footer/Footer';
 import OrderModal from './OrderDetails/OrderModal';
 
 const Products = () => {
-    const [tools, setTools] =useTool();
-    const [order, setOrder]= useState(null)
-    
+    const [tools, setTools] = useTool();
+    const [order, setOrder] = useState(null)
+
     return (
-             <div>
+        <div>
             <h1 className=' my-4 text-5xl font-bold'>Our Parts</h1>
 
             <div className='grid lg:grid-cols-3 gap-4 mt-4 justify-items-center'>
-                   {
-                tools.map (tool => <Tool
-                key={tool.id}
-                tool = {tool}
-               
-                ></Tool>)
-            }
+                {
+                    tools.map(tool => <Tool
+                        key={tool.id}
+                        tool={tool}
+                        setOrder={setOrder}
+                    ></Tool>)
+                }
             </div>
-            {order && <OrderModal 
-              order={order}
-              setOrder= {setOrder}
-              ></OrderModal>}
-            
-            <Footer></Footer>
+            {order && <OrderModal
+                order={order}
+                setOrder={setOrder}
+            ></OrderModal>}
+
+            <div>
+               
+                {order && <OrderModal
+                    order={order}
+                    setOrder={setOrder}
+                ></OrderModal>}
+
+                <Footer></Footer>
+            </div>
+
         </div>
-      
-        
+
+
+
     );
 };
 
